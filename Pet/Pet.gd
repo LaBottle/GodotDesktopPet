a390ff781afcb4cdb468440daec7f767da93b546
LaBottle <191100341@mail.dhu.edu.cn>
2022-11-30 12:59:10 +0800

    Pet movement

--- Pet/Pet.gd
+++ Pet/Pet.gd
@@ -1,97 +1,132 @@
-extends KinematicBody2D
+extends AnimatedSprite
 
-onready var AnimatedSprite = $AnimatedSprite
-onready var Timer = $StateSwitchTimer
+onready var animation_player: AnimationPlayer = $AnimationPlayer
+onready var action_switch_timer: Timer = $ActionSwitchTimer
 
-enum {RELEASED, DRAGGING, LANDING, IDLE}
+enum State {RELEASED, DRAGGING, ACTION}
+enum Action {IDLE, WALK, RUN}
 
-var random = RandomNumberGenerator.new()
-var state := RELEASED
-var velocity := Vector2.ZERO
-var gravity :float = ProjectSettings.get("physics/2d/default_gravity")
-var click_pos := Vector2.ZERO
+
+#constant
+const walk_speed := 200
+const run_speed := 400
+const speed_in_wall := 150
 var screen_size := OS.get_screen_size()
-var offset := Vector2(60, 35)
-# Called when the node enters the scene tree for the first time.
+const window_offset := Vector2(60, 35)
+var gravity :float = ProjectSettings.get("physics/2d/default_gravity")
+
+#variable
+var state setget set_state, get_state
+var action setget set_action, get_action
+var velocity := Vector2.ZERO
+var click_pos := Vector2.ZERO
+var direction = 1
 
 func _ready() -> void:
-	random.randomize()
-	state = DRAGGING
-	pass # Replace with function body.
+	randomize()
+	self.state = State.RELEASED
 
 
 func _physics_process(delta: float) -> void:
-	print(OS.window_position)
-	match state:
-		DRAGGING:
-			#进行拖拽
-			if Input.is_action_pressed("click"):
-				OS.set_window_position(OS.window_position + get_global_mouse_position() - click_pos - offset)
-				
-			#取消拖拽
+	print(action_switch_timer.time_left)
+	match self.state:
+		State.DRAGGING:
+			#TO RELEASED
+			if not Input.is_action_pressed("click"):
+				self.state = State.RELEASED
 			else:
-				state = RELEASED
-				AnimatedSprite.play("release")
+				OS.set_window_position(OS.window_position + get_global_mouse_position() - click_pos - window_offset)
+
 				
-		RELEASED:
-			#被拖拽
+		State.RELEASED:
+			#TO DEAGGING
 			if Input.is_action_just_pressed("click"):
-				velocity = Vector2.ZERO
-				click_pos = get_local_mouse_position()
-				state = DRAGGING
-				AnimatedSprite.play("drag")
-				continue
-				
-			#落地
-			if OS.window_position.y + 75 >= screen_size.y:
-				OS.window_position.y = screen_size.y - 75
-				velocity.y = 0
-				#开启计时器，让land动作完整
-				Timer.wait_time = 3.0
-				Timer.start(1)
-				state = LANDING
-				AnimatedSprite.play("land")
-				
-			#状态不改变
+				self.state = State.DRAGGING
+			#TO ACTION
+			elif OS.window_position.y + 75 >= screen_size.y:
+				self.state = State.ACTION
 			else:
 				velocity.y += gravity * delta
 				OS.window_position = OS.window_position.move_toward(OS.window_position + velocity*delta, INF)
 				
-		LANDING:
+#		LANDING:
+#			#被拖拽
+#			if Input.is_action_just_pressed("click"):
+#				self.state = DRAGGING
+#				animated_sprite.play("drag")
+#
+#
+#			#land动画结束
+#			if state_switch_timer.is_stopped():
+#				#重启计时器
+#				state_switch_timer.wait_time = 5.0
+#				state_switch_timer.start(1)
+#				self.state = IDLE
+#				animated_sprite.play("idle")
+				
+		State.ACTION:
 			#被拖拽
 			if Input.is_action_just_pressed("click"):
-				state = DRAGGING
-				AnimatedSprite.play("drag")
-				continue
-				
-			#land动画结束
-			if Timer.is_stopped():
-				#重启计时器
-				Timer.wait_time = 5.0
-				Timer.start(1)
-				state = IDLE
-				AnimatedSprite.play("idle")
-				
-		IDLE:
-			#被拖拽
-			if Input.is_action_just_pressed("click"):
-				state = DRAGGING
-				AnimatedSprite.play("drag")
-				continue
+				self.state = State.DRAGGING
+				return
+			else:
+				OS.window_position = OS.window_position.move_toward(OS.window_position + velocity * delta, INF)
+
+
+
+func set_state(new_state) -> void:
+	state = new_state
+	match new_state:
+		State.RELEASED:
+			animation_player.play("release")
+		State.DRAGGING:
+			action_switch_timer.stop()
+			velocity = Vector2.ZERO
+			click_pos = get_local_mouse_position()
+			animation_player.play("drag")
+		State.ACTION:
+			OS.window_position.y = screen_size.y - 75
+			velocity.y = 0
+			animation_player.play("land") # will set action to idle
+
 			
-			#状态不变	
-			if Timer.time_left == 0:
-				print("finsh")
-				match random.randi()%10:
-					1:
-						Timer.wait_time = 3.0
-						AnimatedSprite.play("walk")
-					2:
-						Timer.wait_time = 1.0
-						AnimatedSprite.play("run")
-					_:
-						Timer.wait_time = 10.0
-						AnimatedSprite.play("idle")
-				Timer.start(1)
-			pass
-			
+func get_state() -> int:
+	return state
+	
+func set_action(new_action) -> void:
+	action = new_action
+
+	match new_action:
+		Action.IDLE:
+			velocity = Vector2.ZERO
+			action_switch_timer.start(2 + 2 * randf()) # 2 ~ 4
+			animation_player.play("idle")
+		Action.WALK:
+			if randi() % 2 == 1:
+				direction = 1
+				flip_h = false
+			else:
+				direction = -1
+				flip_h = true
+			velocity = Vector2(walk_speed * direction, 0)
+			action_switch_timer.start(4 + 2 * randf()) # 4 ~ 6
+			animation_player.play("walk")
+		Action.RUN:
+			if randi() % 4 == 1:
+				direction = 1
+				flip_h = false
+			else:
+				direction = -1
+				flip_h = true
+			velocity = Vector2(run_speed * direction, 0)
+			action_switch_timer.start(1.5 + randf()) # 1.5 ~ 2.5
+			animation_player.play("walk")
+
+func get_action() -> int:
+	return action
+
+func _on_ActionSwitchTimer_timeout() -> void:
+	var next_action = randi() % (Action.size() - 1)
+	if next_action >= action:
+		next_action += 1
+	self.action = next_action
--- World.gd
+++ World.gd
@@ -1,28 +1,4 @@
 extends Node2D
 
-onready var pet_rect :ReferenceRect = $Pet/ReferenceRect
-enum {RELEASED, CLICKED, DRAGGING}
-
-var offset := Vector2.ZERO
-var click_pos := Vector2.ZERO
 func _ready() -> void:
 	get_viewport().transparent_bg = true
-	#OS.execute("powershell.exe", ["-Command", "./7-Zip/7z.exe", "x", "ost.7z"], false)
-	
-#	if Input.is_action_just_pressed("click"):
-#		$Pet.statu = RELEASED
-#		click_pos = get_local_mouse_position()
-#	if Input.is_action_pressed("click"):
-#		$Pet.statu = DRAGGING
-#		OS.set_window_position(OS.window_position + get_global_mouse_position() - click_pos)
-#	if event.is_action_pressed("click"):
-#		if Rect2(pet_rect.rect_global_position, pet_rect.rect_size).has_point(get_global_mouse_position()):
-#			statu = CLICKED
-#	if statu == CLICKED and event.is_class("InputEventMouseMotion"):
-#		statu = DRAGGING
-#	if statu == DRAGGING:
-#		if event.is_action_released("click"):
-#			statu = RELEASED
-#		else:
-#			get_viewport().set
-#			global_position = get_global_mouse_position() + offset
